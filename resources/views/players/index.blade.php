59c13fd8f264d7f95a495f74868a14dbe6f96258
Sapna Bechoe <s.bechoe@rocmondriaan.nl>
2024-02-12 23:03:29 +0100

    first look players overview

--- resources/views/players/index.blade.php
+++ resources/views/players/index.blade.php
@@ -6,9 +6,76 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    <h1>Players</h1>
-    <div>index</div>
+<div class="w-full h-lvh flex justify-center items-center bg-red-950">
+    <div class="max-w-sm m-6 border-2 p-4 rounded-2xl bg-white shadow-2xl shadow-gray-950/80">
+        <img class="rounded-t-lg mx-auto" src="https://cdn.futbin.com/content/fifa24/img/players/238382.png?v=23" alt=""/>
+        <div class="p-5">
+            <a href="#">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Alessandro Del Piero</h5>
+            </a>
+            <ul class="max-w-md space-y-1 list-disc list-inside">
+                <li>
+                    Club: Juventus
+                </li>
+                <li>
+                    Nationality: Italy
+                </li>
+                <li>
+                    Age: 49
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="max-w-sm m-6 border-2 p-4 rounded-2xl bg-white shadow-2xl shadow-gray-950/80">
+            <img class="rounded-t-lg mx-auto"
+                 src="https://cdn.futbin.com/content/fifa24/img/players/228702.png?v=23"
+                 alt=""/>
+        </a>
+        <div class="p-5">
+            <a href="#">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Frenkie de Jong</h5>
+            </a>
+            <ul class="max-w-md space-y-1 list-disc list-inside">
+                <li>
+                    Club: Barcelona
+                </li>
+                <li>
+                    Nationality: Netherlands
+                </li>
+                <li>
+                    Age: 26
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="max-w-sm m-6 border-2 p-4 rounded-2xl bg-white shadow-2xl shadow-gray-950/80">
+            <img class="rounded-t-lg mx-auto"
+                 src="https://cdn.futbin.com/content/fifa15/img/players/1179.png?v=23"
+                 alt=""/>
+        </a>
+        <div class="p-5">
+            <a href="#">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Gianluigi Buffon</h5>
+            </a>
+            <ul class="max-w-md space-y-1 list-disc list-inside">
+                <li>
+                    Club: Juventus
+                </li>
+                <li>
+                    Nationality: Italy
+                </li>
+                <li>
+                    Age: 46
+                </li>
+            </ul>
+        </div>
+    </div>
+
+</div>
 </body>
 </html>
