3f6e13caed0aaa68196deb60d259e9e56ddd710a
Sapna Bechoe <s.bechoe@rocmondriaan.nl>
2024-02-16 21:51:11 +0100

    Create player 50%

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -3,16 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Player;
+
 
 class PlayerController extends Controller
 {
     public function index()
     {
-        return view('players.index');
+        $players = Player::all();
+        return view('players.index', ['players' => $players]);
     }
 
     public function create()
     {
         return view('players.create');
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'nationality' => 'required',
+        ]);
+
+        $newPlayer = Player::create($data);
+        return redirect(route('players.index'));
+//       dd($request); // This will dump the request object
     }
 }
